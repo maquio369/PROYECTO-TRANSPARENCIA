7e5b4eb663e7465a80a0a63b22ee011fe7c56057
Saladin0? <maquio92@gmail.com>
2025-09-10 10:11:58 -0600

    cambio de base

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ export DJANGO_SETTINGS_MODULE=repositorio_articulo65.settings_docker
 
 # Esperar a que PostgreSQL esté disponible
 echo "Esperando a PostgreSQL..."
-until python -c "import psycopg2; psycopg2.connect(host='$DB_HOST', port='$DB_PORT', user='$DB_USER', password='$DB_PASSWORD', dbname='$DB_NAME')" 2>/dev/null; do
+until python -c "import psycopg2; psycopg2.connect(host='172.16.35.75', port='32768', user='maquio', password='maquio92', dbname='repositorio_transparencia')" 2>/dev/null; do
   echo "Esperando conexión a la base de datos..."
   sleep 2
 done
